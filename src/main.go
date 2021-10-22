3c2aadc202e6907c5b55e0f9d5fc9b7905bc3656
Bismarck <sam@aepearce.com>
2021-10-23 00:32:44 +0200

    Accidentally left debug mode enabled

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,7 +12,7 @@ func main() {
 	LoadTemplates(e)
 
 	// For local debugging
-	e.Logger.Fatal(e.Start(":5000"))
+	//e.Logger.Fatal(e.Start(":5000"))
 
-	//e.Logger.Fatal(e.StartTLS(":"+os.Getenv("WISHLIST_FE_PORT"), os.Getenv("WISHLIST_SSL_CERT"), os.Getenv("WISHLIST_SSL_KEY")))
+	e.Logger.Fatal(e.StartTLS(":"+os.Getenv("WISHLIST_FE_PORT"), os.Getenv("WISHLIST_SSL_CERT"), os.Getenv("WISHLIST_SSL_KEY")))
 }
