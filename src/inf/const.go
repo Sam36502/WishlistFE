7e1dd0b601236e8ebc6a35ee092db3b26baabdeb
Bismarck <sam@aepearce.com>
2022-05-21 19:52:36 +0200

    Removed invalid API URL

--- src/inf/const.go
+++ src/inf/const.go
@@ -4,3 +4,8 @@ package inf
 const (
 	COOKIE_TOKEN_KEY = "token"
 )
+
+// Wishlist Lib Config
+const (
+	WISHLIST_BASE_URL = "https://www.pearcenet.ch"
+)
--- src/inf/wishlist.go
+++
@@ -1,5 +0,0 @@
-package inf
-
-const (
-	WISHLIST_BASE_URL = "https://www.pearcenet.ch:2512"
-)
