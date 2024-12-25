350b6a173cc2dba9093ea89545b452cea96dfdcc
Bismarck <sam@aepearce.com>
2024-12-25 03:53:50 +0100

    Added basic user-list filtering

--- src/handlers/ptl_itemlist.go
+++ src/handlers/ptl_itemlist.go
@@ -25,16 +25,34 @@ func PtlUserList(c echo.Context) error {
 		return echo.ErrNotFound
 	}
 
+	show_received := c.QueryParams().Get("cb_show_received") == "on"
+	show_reserved := c.QueryParams().Get("cb_show_reserved") == "on"
+
 	wish := inf.GetWishlistClient(wishlistlib.Token{})
 
 	user, err := wish.GetUserByEmail(email)
 	if err != nil {
 		return c.Redirect(http.StatusPermanentRedirect, "/home")
 	}
-	items, err := wish.GetAllItemsOfUser(user)
+
+	// TODO: Do item filtering on db-side
+	allItems, err := wish.GetAllItemsOfUser(user)
 	if err != nil {
 		fmt.Println("[ERROR] Failed to retrieve User's items:\n ", err)
 		return c.Redirect(http.StatusPermanentRedirect, "/err/500.html")
+	}
+
+	// For now: Filtering in-memory here
+	items := make([]wishlistlib.Item, 0)
+	for _, item := range allItems {
+		skip := (!show_received && item.Status.StatusID == inf.STATUS_RECEIVED) ||
+			(!show_reserved && item.Status.StatusID == inf.STATUS_RESERVED)
+
+		if skip {
+			continue
+		}
+
+		items = append(items, item)
 	}
 
 	// Check if currently logged in as this user
